84aa5a9ca63c6234b2dbf64865224d80b8d2cb99
m15927509890@163.com <m15927509890@163.com>
2020-12-10 15:44:12 +0800

    增加测试数据

--- CNet/Test.c
+++ CNet/Test.c
@@ -5,10 +5,30 @@
 #include"Matrix.h"
 #include"Net.h"
 #include"Data.h"
+#include"weights.h"
+
 void main() {
 	clock_t start, finish;
 	double total_time;
 	start = clock();
+
+	for (int k = 0; k < 53; k++) {
+		double test_input_batch[DATA_BATCH][DATA_DIMENSION];
+		for (int i = 0; i < DATA_BATCH; i++) {
+			for(int j = 0; j < DATA_DIMENSION; j++) {
+				test_input_batch[i][j] = test_input[k * DATA_BATCH + i][j];
+			}
+		}
+		int* res = (int*)calloc(DATA_BATCH, sizeof(int));
+		if (res != NULL) {
+			netCalculate(test_input_batch, res);
+			for (int i = 0; i < DATA_BATCH; i++) {
+				printf("%3d", res[i]);
+			}
+			free(res);
+		}
+	}
+	
 
 
 	finish = clock();
